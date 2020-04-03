a639b3490334d73493298fbf0b458c020f200759
Dominik Stiller <domi.stiller@gmail.com>
2020-04-03 14:38:05 +0200

    Split data into test and train

---
+++ scripts/split_data.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+data = pd.read_csv('data/dataset_full.csv')
+
+while True:
+    train, test = train_test_split(data, test_size=.2, shuffle=True)
+    # Try until train part contains all values from 0 to 7 for column 'Kinder'
+    # Necessary because there are not many samples with high numbers of children
+    if all([n in train['Kinder'].values for n in range(0, 8)]):
+        break
+
+train.to_csv('data/dataset_train.csv', index=False)
+test.to_csv('data/dataset_test.csv', index=False)
