444ccca0591b38dc052b1c2ddd975cade719c29f
Dominik Stiller <domi.stiller@gmail.com>
2020-05-08 23:36:17 +0200

    Add report

--- insurance_advisor/__main__.py
+++ insurance_advisor/__main__.py
@@ -16,8 +16,8 @@ def train(train_data):
     advisor.save_model()
 
 
-def evaluate(model_id, evaluate_data):
-    features, prediction = InsuranceAdvisor.load_data(evaluate_data)
+def evaluate(model_id, test_data):
+    features, prediction = InsuranceAdvisor.load_data(test_data)
 
     advisor = InsuranceAdvisor()
     advisor.load_model(model_id)
@@ -25,7 +25,7 @@ def evaluate(model_id, evaluate_data):
     prediction_actual = advisor.predict(features)
     print(classification_report(prediction, prediction_actual))
 
-    plot_confusion_matrix(advisor, features, prediction, values_format='d', cmap='Blues')
+    plot_confusion_matrix(advisor, features, prediction, values_format='d', cmap='Blues', normalize='None')
     plt.savefig('confusion_matrix.png')
     print('Confusion matrix saved to "confusion_matrix.png"')
 
@@ -47,7 +47,7 @@ if __name__ == '__main__':
         print('Usage: python insurance_advisor [command]')
         print('Available commands:')
         print('- train [train-data]')
-        print('- evaluate [model-id] [evaluate-data]')
+        print('- evaluate [model-id] [test-data]')
         print('- predict [model-id] [predict-data]')
         exit(1)
 
